867f5a23b43d0994c146855f25c40e3cf0b41d16
Semir Suljevic <suljevic.semir@gmailcom>
2024-04-20 01:07:58 +0200

    setup(signing): signed and configured app with dev and prod flavor feature/setup

---
+++ app/KeystoreConstants.kt
@@ -0,0 +1,13 @@
+object KeystoreConstants {
+    val devStorePassword = "devStorePassword"
+    val devKeyPassword = "devAlias"
+    val devKeyAlias = "devKeyAlias"
+    val devStoreFile = "devStoreFile"
+
+    val prodStorePassword = "prodStorePassword"
+    val prodKeyPassword = "prodKeyPassword"
+    val prodKeyAlias = "prodKeyAlias"
+    val prodStoreFile = "prodStoreFile"
+
+
+}
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,41 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.kotlinAndroid)
 }
 
+//load signing values into variable, use to create signing config later
+val keystorePropertiesFile: File = rootProject.file("keystore.properties")
+val keystoreProperties = Properties()
+keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+
+object SigningConstants {
+    const val devStorePassword = "devStorePassword"
+    const val devKeyPassword = "devAlias"
+    const val devKeyAlias = "devKeyAlias"
+    const val devStoreFile = "devStoreFile"
+
+    const val prodStorePassword = "prodStorePassword"
+    const val prodKeyPassword = "prodKeyPassword"
+    const val prodKeyAlias = "prodKeyAlias"
+    const val prodStoreFile = "prodStoreFile"
+
+    const val devFlavor = "dev"
+    const val prodFlavor = "prod"
+    const val flavorDimension = "demo"
+}
+
+
+
 android {
     namespace = "com.example.hechimdemo"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.hechimdemo"
+
         minSdk = 26
         targetSdk = 34
         versionCode = 1
@@ -27,6 +53,43 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+
+    signingConfigs {
+        create(SigningConstants.devFlavor) {
+            storeFile = file(SigningConstants.devStoreFile)
+            storePassword = keystoreProperties.getProperty(SigningConstants.devStorePassword)
+            keyAlias = keystoreProperties.getProperty(SigningConstants.devKeyAlias)
+            keyPassword = keystoreProperties.getProperty(SigningConstants.devKeyPassword)
+        }
+        create(SigningConstants.prodFlavor) {
+            storeFile = file(SigningConstants.prodStoreFile)
+            storePassword = keystoreProperties.getProperty(SigningConstants.prodStorePassword)
+            keyAlias = keystoreProperties.getProperty(SigningConstants.prodKeyAlias)
+            keyPassword = keystoreProperties.getProperty(SigningConstants.prodKeyPassword)
+        }
+    }
+
+
+    flavorDimensions.add(SigningConstants.flavorDimension)
+    productFlavors {
+        create(SigningConstants.devFlavor) {
+            dimension = flavorDimensions.first()
+            applicationId = "com.semirsuljevic.hechim"
+            applicationIdSuffix = ".dev"
+            buildConfigField("String", "BASE_URL", "\"TBD\"")
+            signingConfig = signingConfigs.getAt(SigningConstants.devFlavor)
+        }
+
+        create(SigningConstants.prodFlavor) {
+            dimension = flavorDimensions.first()
+            applicationId = "ba.semirsuljevic.hechim"
+            buildConfigField("String", "BASE_URL", "\"TBD\"")
+            signingConfig = signingConfigs.getAt(SigningConstants.prodFlavor)
+        }
+    }
+
+
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -36,6 +99,7 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
