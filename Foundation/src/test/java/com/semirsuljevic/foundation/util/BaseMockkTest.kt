0a87ba37587f5df81e002e176bab165dcc76abe5
Semir Suljevic <suljevic.semir@gmailcom>
2024-05-02 14:11:22 +0200

    feat(test): added test setup feature/onboarding

---
+++ Foundation/src/test/java/com/semirsuljevic/foundation/util/BaseMockkTest.kt
@@ -0,0 +1,26 @@
+package com.semirsuljevic.foundation.util
+
+import io.mockk.MockKAnnotations
+import io.mockk.clearAllMocks
+import io.mockk.unmockkAll
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.extension.ExtendWith
+
+@ExtendWith(TestMainDispatcherExtension::class)
+abstract class BaseMockkTest<T> {
+    @BeforeEach
+    fun setupMockk() {
+        MockKAnnotations.init(this, relaxUnitFun = true)
+    }
+
+    @AfterEach
+    fun tearDownMockk() {
+        clearAllMocks()
+        unmockkAll()
+    }
+
+    abstract fun createSut(): T
+
+    abstract fun setUp()
+}
