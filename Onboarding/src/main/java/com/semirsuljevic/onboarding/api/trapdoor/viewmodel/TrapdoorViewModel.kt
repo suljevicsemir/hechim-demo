fefee8417c50d5cbed79615a776f4960ef50381d
Semir Suljevic <suljevic.semir@gmailcom>
2024-05-17 12:31:07 +0200

    feat(trapdoor): added trapdoor feature/trapdoor

---
+++ Onboarding/src/main/java/com/semirsuljevic/onboarding/api/trapdoor/viewmodel/TrapdoorViewModel.kt
@@ -0,0 +1,115 @@
+package com.semirsuljevic.onboarding.api.trapdoor.viewmodel
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.provider.Settings
+import androidx.activity.compose.ManagedActivityResultLauncher
+import androidx.activity.result.ActivityResult
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.viewModelScope
+import com.semirsuljevic.foundation.api.authentication.HechimAuthentication
+import com.semirsuljevic.foundation.api.datastore.PermissionsRequestsProvider
+import com.semirsuljevic.onboarding.api.permissions.config.PermissionConstants
+import com.semirsuljevic.onboarding.api.permissions.viewmodel.PermissionsProviderViewModel
+import com.semirsuljevic.onboarding.api.trapdoor.config.TrapdoorConfig
+import com.semirsuljevic.onboarding.api.trapdoor.config.TrapdoorConstants
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+/**
+ViewModel responsible of trapdoor screens.
+ * @property checkTrapdoorPermission - main method responsible for pushing trapdoor screens
+ * @property fixPermission - navigates user to the permission solution
+ * @property trapdoorConfig - controls the modal state
+ */
+
+@HiltViewModel
+class TrapdoorViewModel @Inject constructor(
+    private val authenticationApi: HechimAuthentication,
+    private val permissionRequestsProvider: PermissionsRequestsProvider,
+    @ApplicationContext private val applicationContext: Context
+): PermissionsProviderViewModel(applicationContext = applicationContext){
+
+    private val _trapdoorConfig = mutableStateOf<TrapdoorConfig?>(null)
+    val trapdoorConfig get() = _trapdoorConfig
+
+    /**
+    Checks necessary permissions on every app launch or every time app is brought to foreground.
+    It is invoked from a Lifecycle observer Composable's onResume event (which covers app start as well)
+    Order of show priority: Location - Battery Optimization - Physical Activity
+    Reversed order of checking as the last satisfied condition will be shown.
+    Will be called when user returns from app settings to app - and refresh modal (or close it).
+     */
+    fun checkTrapdoorPermission() {
+        //no need to check for trapdoors if user isn't authenticated
+        //this prevents modal invocation for onboarding / auth flow
+        //in order for trapdoor modal to kick in
+        val value: MutableList<String> = mutableListOf()
+        //set to null each time method is triggered - enables to refresh the latest trapdoor value
+        _trapdoorConfig.value = null
+        viewModelScope.launch {
+            val requests = permissionRequestsProvider.getPermissionRequests().first()
+            if (!requests.finishedPermissions || !authenticationApi.isAuthenticated()) {
+                return@launch
+            }
+            //necessary to add small delay as trapdoor is checked on start and foreground
+            delay(TrapdoorConstants.trapdoorDelay)
+
+            //Activity recognition for Android 10 and above
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q &&
+                !permissionGranted(android.Manifest.permission.ACTIVITY_RECOGNITION)) {
+                value.add(android.Manifest.permission.ACTIVITY_RECOGNITION)
+                _trapdoorConfig.value = TrapdoorConstants.activityRecognition
+            }
+            //Battery Optimization for Samsung devices
+            if(!checkBatteryOptimization()) {
+                value.add(android.Manifest.permission.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS)
+                _trapdoorConfig.value = TrapdoorConstants.batteryOptimization
+            }
+            //Android 9 and below location
+            if(Build.VERSION.SDK_INT <= Build.VERSION_CODES.P &&
+                !permissionGranted(android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+                value.add(android.Manifest.permission.ACCESS_FINE_LOCATION)
+                _trapdoorConfig.value = TrapdoorConstants.location
+            }
+            //Background location for Android 10 and above
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q &&
+                !permissionGranted(android.Manifest.permission.ACCESS_BACKGROUND_LOCATION)) {
+                value.add(android.Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+                _trapdoorConfig.value = TrapdoorConstants.locationAlways
+            }
+            println("checked trapdoor")
+            println(_trapdoorConfig.value)
+        }
+    }
+
+    /**
+    Invoked from button on Trapdoor screen, if the trapdoor config is for battery optimization,
+    request for ignore is invoked.
+    Otherwise redirects the user to app settings.
+     */
+    fun fixPermission(launcher: ManagedActivityResultLauncher<Intent, ActivityResult>) {
+        if(_trapdoorConfig.value != null){
+            if(trapdoorConfig.value?.manifestPermission == android
+                    .Manifest.permission.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS) {
+                requestIgnoreBatteryOptimization()
+                return
+            }
+            //redirect the user to app settings
+            launcher.launch(
+                Intent(
+                    Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                    Uri.fromParts(TrapdoorConstants.packageConstant, applicationContext.packageName, null)
+                )
+            )
+        }
+    }
+}
--- app/src/main/java/com/semirsuljevic/hechimdemo/ui/AppNavigator.kt
+++ app/src/main/java/com/semirsuljevic/hechimdemo/ui/AppNavigator.kt
@@ -3,6 +3,8 @@ package com.semirsuljevic.hechimdemo.ui
 import androidx.activity.ComponentActivity
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelStoreOwner
@@ -13,7 +15,9 @@ import androidx.navigation.compose.rememberNavController
 import com.semirsuljevic.hechimdemo.viewmodel.MainViewModel
 import com.semirsuljevic.onboarding.api.permissions.navigation.permissionsNavGraph
 import com.semirsuljevic.onboarding.api.permissions.viewmodel.PermissionViewModel
+import com.semirsuljevic.onboarding.api.trapdoor.viewmodel.TrapdoorViewModel
 import com.semirsuljevic.onboarding.api.welcome.navigation.OnBoardingNavGraph
+import com.semirsuljevic.onboarding.internal.trapdoor.TrapdoorScreen
 import com.semirsuljevic.ui.api.common.ComposableLifecycle
 
 @Composable
@@ -22,20 +26,26 @@ fun AppNavigator(
     viewModelStoreOwner: ViewModelStoreOwner
 ) {
     val context = LocalContext.current
+    val trapdoorViewModel : TrapdoorViewModel = viewModel(context as ComponentActivity)
 
-    val mainViewModel: MainViewModel = viewModel(context as ComponentActivity)
+
+
+    val mainViewModel: MainViewModel = viewModel(context)
     val permissionsViewModel: PermissionViewModel = viewModel(context)
 
     ComposableLifecycle { _, event ->
         //ON_RESUME is triggered: cold start, warm start
         //view model checks if app should check trapdoor (permissions must be finished at least once)
-//        if(event == Lifecycle.Event.ON_RESUME) {
-//            trapdoorViewModel.checkTrapdoorPermission()
-//        }
+        if(event == Lifecycle.Event.ON_RESUME) {
+            trapdoorViewModel.checkTrapdoorPermission()
+        }
         if(event == Lifecycle.Event.ON_CREATE) {
             permissionsViewModel.startPermissions()
         }
     }
+
+    println("trapdoor is: ${trapdoorViewModel.trapdoorConfig.value}")
+
 
 
     LaunchedEffect(Unit) {
@@ -49,4 +59,10 @@ fun AppNavigator(
         OnBoardingNavGraph(viewModelStoreOwner)
         permissionsNavGraph(viewModelStoreOwner)
     }
+
+    TrapdoorScreen(
+        config = trapdoorViewModel.trapdoorConfig.value,
+        trapdoorViewModel = trapdoorViewModel
+    )
+
 }
